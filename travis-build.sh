5f9d1266286afa761f86e92e44c7e14c0212be8c
Graeme Rocher <graemerocher@Graemes-iMac.local>
2016-06-28 15:19:18 +0200

    Pass --refresh-dependencies

--- travis-build.sh
+++ travis-build.sh
@@ -4,9 +4,9 @@ EXIT_STATUS=0
 ./gradlew --stop
 
 
-./gradlew test -no-daemon -x gorm-cassandra-spring-boot:test  || EXIT_STATUS=$?
+./gradlew test --refresh-dependencies -no-daemon -x gorm-cassandra-spring-boot:test  || EXIT_STATUS=$?
 if [[ $EXIT_STATUS -eq 0 ]]; then 
-    ./gradlew gorm-cassandra-spring-boot:test -no-daemon || EXIT_STATUS=$?
+    ./gradlew gorm-cassandra-spring-boot:test --refresh-dependencies -no-daemon || EXIT_STATUS=$?
 fi
 
 ./gradlew --stop
